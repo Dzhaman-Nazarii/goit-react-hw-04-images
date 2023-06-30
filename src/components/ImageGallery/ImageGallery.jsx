73f669109233089ef171494711db0dbca32a28e5
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-06-30 19:08:22 +0300

    changed ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,76 +1,67 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import Loader from 'components/Loader/Loader';
 import css from './ImageGallery.module.css';
 
-export default class ImageGallery extends Component {
-  state = {
-    gallery: [],
-    status: 'idle'
-  };
+export default function ImageGallery({ galleryName, page, onGalleryData }) {
+  const [gallery, setGallery] = useState([]);
+  const [status, setStatus] = useState('idle');
 
-  async componentDidUpdate(prevProps) {
-    const prevName = prevProps.galleryName;
-    const nextName = this.props.galleryName;
-    const prevPage = prevProps.page;
-    const nextPage = this.props.page;
+  useEffect(() => {
+    if (galleryName === '') {
+      return
+    }
 
-    if (prevName !== nextName || prevPage !== nextPage) {
+    const fetchData = async () => {
       try {
-        this.setState({ status: 'pending' });
-
-        if (prevName !== nextName) {
-          this.setState({ gallery: [] });
-        }
+        setStatus('pending');
 
         const response = await fetch(
-          `https://pixabay.com/api/?q=${nextName}&page=${nextPage}&key=37532394-4be77775868909c78ad8f61fa&image_type=photo&orientation=horizontal&per_page=12`
+          `https://pixabay.com/api/?q=${galleryName}&page=${page}&key=37532394-4be77775868909c78ad8f61fa&image_type=photo&orientation=horizontal&per_page=12`
         );
         const galleryData = await response.json();
         const { hits, totalHits } = galleryData;
 
-        this.setState(prevState => ({
-          gallery: [...prevState.gallery, ...hits],
-          status: 'resolved'
-        }));
+        setGallery(prevGallery => [...prevGallery, ...hits]);
+        setStatus('resolved');
 
-        this.props.onGalleryData(hits, totalHits);
+        onGalleryData(hits, totalHits);
       } catch (error) {
-        this.setState({ status: 'rejected' });
+        setStatus('rejected');
       }
+    };
+
+    if (galleryName && (galleryName !== '' || page !== 1)) {
+      fetchData();
     }
+  }, [galleryName, page, onGalleryData]);
+
+  if (status === 'idle') {
+    return null;
   }
 
-  render() {
-    const { gallery, status } = this.state;
+  if (status === 'pending') {
+    return <Loader />;
+  }
 
-    if (status === 'idle') {
-      return null;
-    }
+  if (status === 'rejected') {
+    return <h1>Виникла помилка</h1>;
+  }
 
-    if (status === 'pending') {
-      return <Loader />;
-    }
-
-    if (status === 'rejected') {
-      return <h1>Виникла помилка</h1>;
-    }
-
-    if (status === 'resolved') {
-      return (
-        <ul className={css.ImageGallery}>
-          {gallery.map(({ id, webformatURL, tags, largeImageURL }) => (
-            <ImageGalleryItem
-              key={id}
-              src={webformatURL}
-              alt={tags}
-              largeImageURL={largeImageURL}
-            />
-          ))}
-        </ul>
-      );
-    }
+  if (status === 'resolved') {
+    return (
+      <ul className={css.ImageGallery}>
+        {gallery.map(({ id, webformatURL, tags, largeImageURL }) => (
+          <ImageGalleryItem
+            key={id}
+            src={webformatURL}
+            alt={tags}
+            largeImageURL={largeImageURL}
+          />
+        ))}
+      </ul>
+    );
   }
 }
 
