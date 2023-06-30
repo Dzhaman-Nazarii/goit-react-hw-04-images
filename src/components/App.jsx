0d04f532bf946fbefcc3247fccc383e243ddf170
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-06-30 19:00:21 +0300

    changed App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,20 +5,12 @@ import Button from './Button/Button';
 
 export default function App() {
 
-  // state = {
-  //   galleryName: '',
-  //   page: 1,
-  //   totalHits: 0,
-  //   gallery: []
-  // };
-
   const [galleryName, setGalleryName] = useState('');
   const [page, setPage] = useState(1);
   const [totalHits, setTotalHits] = useState(0);
   const [gallery, setGallery] = useState([]);
 
   const handleFormSubmit = galleryName => {
-    // this.setState({ galleryName, page: 1, totalHits: 0 });
     setGalleryName(galleryName)
     setPage(1)
     setTotalHits(0)
@@ -26,16 +18,11 @@ export default function App() {
 
   const handleButtonMore = () => {
     setPage(prevPage => prevPage.page + 1)
-    // this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   const handleGalleryData = (hits, totalHits) => {
     setGallery(prevGallery => [...prevGallery, ...hits])
     setTotalHits(totalHits)
-    // this.setState(prevState => ({
-    //   gallery: [...prevState.gallery, ...hits],
-    //   totalHits
-    // }));
   };
 
     return (
