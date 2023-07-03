db053d3b7e1aaf85073521c60f749a523c90e6cd
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-07-03 17:50:05 +0300

    changed method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Searchbar from './Searchbar/SearchBar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
@@ -9,6 +9,32 @@ export default function App() {
   const [page, setPage] = useState(1);
   const [totalHits, setTotalHits] = useState(0);
   const [gallery, setGallery] = useState([]);
+  const [status, setStatus] = useState('idle');
+
+    useEffect(() => {
+    if (galleryName === '') {
+      return;
+    }
+
+    const fetchData = async () => {
+      try {
+        setStatus('pending');
+
+        const response = await fetch(
+          `https://pixabay.com/api/?q=${galleryName}&page=${page}&key=37532394-4be77775868909c78ad8f61fa&image_type=photo&orientation=horizontal&per_page=12`
+        );
+        const galleryData = await response.json();
+        const { hits, totalHits } = galleryData;
+
+        setGallery([...gallery, ...hits]);
+        setStatus('resolved');
+      } catch (error) {
+        setStatus('rejected');
+      }
+    };
+
+    fetchData();
+  }, [galleryName, page,gallery]);
 
   const handleFormSubmit = galleryName => {
     setGalleryName(galleryName)
@@ -20,10 +46,10 @@ export default function App() {
     setPage(prevPage => prevPage + 1)
   };
 
-  const handleGalleryData = (hits, totalHits) => {
-    setGallery(prevGallery => [...prevGallery, ...hits])
-    setTotalHits(totalHits)
-  };
+  // const handleGalleryData = (hits, totalHits) => {
+  //   setGallery(prevGallery => [...prevGallery, ...hits])
+  //   setTotalHits(totalHits)
+  // };
 
     return (
       <div>
@@ -31,7 +57,9 @@ export default function App() {
         <ImageGallery
           galleryName={galleryName}
           page={page}
-          onGalleryData={handleGalleryData}
+          onGalleryData
+            status = { status }
+            gallery={ gallery }
         />
         { gallery.length < totalHits && gallery.length > 0 && <Button handleButtonMore={handleButtonMore} />}
       </div>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,46 +1,9 @@
-import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import Loader from 'components/Loader/Loader';
 import css from './ImageGallery.module.css';
 
-export default function ImageGallery({ galleryName, page, onGalleryData }) {
-  const [gallery, setGallery] = useState([]);
-  const [prevGalleryName, setPrevGalleryName] = useState('');
-  const [status, setStatus] = useState('idle');
-
-  useEffect(() => {
-    if (galleryName === '') {
-      return;
-    }
-
-    const fetchData = async () => {
-      try {
-        if (galleryName !== prevGalleryName) {
-          setGallery([]);
-        }
-        setStatus('pending');
-
-        const response = await fetch(
-          `https://pixabay.com/api/?q=${galleryName}&page=${page}&key=37532394-4be77775868909c78ad8f61fa&image_type=photo&orientation=horizontal&per_page=12`
-        );
-        const galleryData = await response.json();
-        const { hits, totalHits } = galleryData;
-
-        setGallery(prevGallery => [...prevGallery, ...hits]);
-        setStatus('resolved');
-
-        onGalleryData(hits, totalHits);
-      } catch (error) {
-        setStatus('rejected');
-      }
-    };
-
-    if (galleryName && (galleryName !== '' || page !== 1)) {
-      fetchData();
-    }
-    setPrevGalleryName(galleryName);
-  }, [galleryName, page,]);
+export default function ImageGallery({status, gallery }) {
 
   if (status === 'idle') {
     return null;
@@ -71,7 +34,6 @@ export default function ImageGallery({ galleryName, page, onGalleryData }) {
 }
 
 ImageGallery.propTypes = {
-  galleryName: PropTypes.string.isRequired,
-  page: PropTypes.number.isRequired,
-  onGalleryData: PropTypes.func.isRequired
+  status: PropTypes.string.isRequired,
+  gallery: PropTypes.array.isRequired
 };
